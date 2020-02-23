fa28ad094bf70d54d55c666fc348eb013068d25f
CppPhil <CppPhil@users.noreply.github.com>
2020-02-23 19:44:43 +0100

    Adjust test_profiler printing to stdout

--- include/test_profiler.hpp
+++ include/test_profiler.hpp
@@ -4,18 +4,20 @@
 namespace cst {
 class test_profiler : public caf::actor_profiler {
 public:
-  void add_actor(const caf::local_actor& self,
+  test_profiler();
+
+  void add_actor(const caf::local_actor& actor,
                  const caf::local_actor* parent) override;
 
-  void remove_actor(const caf::local_actor& self) override;
+  void remove_actor(const caf::local_actor& actor) override;
 
-  void before_processing(const caf::local_actor& self,
+  void before_processing(const caf::local_actor& actor,
                          const caf::mailbox_element& element) override;
 
-  void after_processing(const caf::local_actor& self,
+  void after_processing(const caf::local_actor& actor,
                         caf::invoke_message_result result) override;
 
-  void before_sending(const caf::local_actor& self,
+  void before_sending(const caf::local_actor& actor,
                       caf::mailbox_element& element) override;
 
   void before_sending_scheduled(const caf::local_actor& self,
--- src/test_profiler.cpp
+++ src/test_profiler.cpp
@@ -6,82 +6,47 @@
 #include "test_tracing_data.hpp"
 
 namespace cst {
-namespace {
-int vasprintf(std::string* strp, const char* fmt, std::va_list ap) noexcept {
-  std::va_list args2;
-  va_copy(args2, ap);
-
-  const auto err_code = std::vsnprintf(nullptr, 0, fmt, ap);
-
-  if (err_code < 0)
-    return err_code;
-
-  const auto byte_count = 1U + err_code;
-
-  strp->resize(byte_count);
-  const auto ret_val = std::vsnprintf(strp->data(), byte_count, fmt, args2);
-
-  va_end(args2);
-  strp->resize(byte_count - 1);
-
-  return ret_val;
+test_profiler::test_profiler() {
+  setbuf(stdout, nullptr);
 }
 
-int asprintf(std::string* strp, const char* fmt, ...) noexcept {
-  std::va_list args1;
-  va_start(args1, fmt);
-
-  const auto ret_val = vasprintf(strp, fmt, args1);
-  va_end(args1);
-
-  return ret_val;
-}
-} // namespace
-
-#define P(...)                                                                 \
-  do {                                                                         \
-    std::string s;                                                             \
-    asprintf(&s, "%s: ", __PRETTY_FUNCTION__);                                 \
-    std::string s2;                                                            \
-    asprintf(&s2, __VA_ARGS__);                                                \
-    std::printf("%s\n", (s + s2).data());                                      \
-  } while ((void) 0, 0)
-
-void test_profiler::add_actor([[maybe_unused]] const caf::local_actor& self,
+void test_profiler::add_actor(const caf::local_actor& self,
                               const caf::local_actor* parent) {
-  P("parent: %s", parent);
+  printf("add_actor: actor = \"%s\", parent = \"%s\"\n", self.name(),
+         parent != nullptr ? parent->name() : "null");
 }
 
-void test_profiler::remove_actor([
-  [maybe_unused]] const caf::local_actor& self) {
-  P("");
+void test_profiler::remove_actor(const caf::local_actor& actor) {
+  printf("remove_actor: actor = \"%s\"\n", actor.name());
 }
 
-void test_profiler::before_processing(
-  [[maybe_unused]] const caf::local_actor& self,
-  const caf::mailbox_element& element) {
-  P("%s", element.content().stringify().data());
+void test_profiler::before_processing(const caf::local_actor& actor,
+                                      const caf::mailbox_element& element) {
+  printf("before_processing: actor = \"%s\", element: \"%s\"\n", actor.name(),
+         element.content().stringify().c_str());
 }
 
-void test_profiler::after_processing(
-  [[maybe_unused]] const caf::local_actor& self,
-  caf::invoke_message_result result) {
-  P("%s", caf::to_string(result).data());
+void test_profiler::after_processing(const caf::local_actor& actor,
+                                     caf::invoke_message_result result) {
+  printf("after_processing: actor = \"%s\", result: \"%s\"\n", actor.name(),
+         caf::to_string(result).c_str());
 }
 
-void test_profiler::before_sending(const caf::local_actor& self,
+void test_profiler::before_sending(const caf::local_actor& actor,
                                    caf::mailbox_element& element) {
-  P("%s", element.content().stringify().data());
+  printf("before_sending: actor = \"%s\", element: \"%s\"\n", actor.name(),
+         element.content().stringify().c_str());
 
-  element.tracing_id = std::make_unique<test_tracing_data>(self.name());
+  element.tracing_id = std::make_unique<test_tracing_data>(actor.name());
 }
 
 void test_profiler::before_sending_scheduled(
-  const caf::local_actor& self,
+  const caf::local_actor& actor,
   [[maybe_unused]] caf::actor_clock::time_point timeout,
   caf::mailbox_element& element) {
-  P("%s", element.content().stringify().data());
+  printf("before_sending_scheduled: actor = \"%s\", element: \"%s\"\n",
+         actor.name(), element.content().stringify().c_str());
 
-  element.tracing_id = std::make_unique<test_tracing_data>(self.name());
+  element.tracing_id = std::make_unique<test_tracing_data>(actor.name());
 }
 } // namespace cst
