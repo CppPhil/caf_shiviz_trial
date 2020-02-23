d607b08a33ea8415133f7971dda27237c8bd99eb
CppPhil <CppPhil@users.noreply.github.com>
2020-02-23 19:21:32 +0100

    Update

--- scripts/build_caf.sh
+++ scripts/build_caf.sh
@@ -11,11 +11,17 @@ cd $ROOT_DIR/external
 
 cd ./actor-framework
 
+cp ./CMakeLists.txt ./CMakeLists.txt.backup
+rm -f ./CMakeLists.txt
+cp $ROOT_DIR/CustomCafCMakeLists.txt ./CMakeLists.txt
+
 ./configure --libs-only --with-actor-profiler
 
 cd ./build
 
 make -j$(nproc)
+
+mv $ROOT_DIR/external/actor-framework/CMakeLists.txt.backup $ROOT_DIR/external/actor-framework/CMakeLists.txt
 
 cd $PREVIOUS_DIRECTORY
 
