f484e88d2781b9ec7aa1e0a73ef11254eec55c2f
CppPhil <CppPhil@users.noreply.github.com>
2020-03-15 12:18:59 +0100

    Add ccache

--- scripts/build_caf.sh
+++ scripts/build_caf.sh
@@ -13,9 +13,9 @@ cd $ROOT_DIR/external
 
 cd ./actor-framework
 
-./configure --build-dir=$CAF_BUILD_DIR --build-type=Debug --no-examples --no-python --with-log-level=TRACE --with-actor-profiler --with-runtime-checks
+CC="ccache gcc" CXX="ccache g++" ./configure --build-dir=$CAF_BUILD_DIR --build-type=Debug --no-examples --no-python --with-log-level=TRACE --with-actor-profiler --with-runtime-checks
 cd $CAF_BUILD_DIR
-make -j$(nproc)
+CC="ccache gcc" CXX="ccache g++" make -j$(nproc)
 
 cd $PREVIOUS_DIRECTORY
 
